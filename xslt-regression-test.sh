acdeebbd1125c724f5ab29cb5b9ba85730686c99
Svante Schubert <svante.schubert@gmail.com>
2023-07-20 18:27:43 +0200

    Adding some test files for Draw to SVG

--- xslt-regression-test.sh
+++ xslt-regression-test.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 # test input, ouput and reference follow a pattern according to the test name and ODF version:
-TEST_NAMES=("tdf95662-complex" "tdf95662" "frames-without-wrap" "floating_image_as_character" "frames-without-wrap" "tdf146264" "floating" "spec-fragment" "default-styles" "tdf156093-image" "RedSquare" "DefaultSquare" "tdf156093-image_alignment")
+TEST_NAMES=("rounded-rectangle-red" "rectangleWithText" "DefaultSquare" "tdf95662-complex" "tdf95662" "frames-without-wrap" "floating_image_as_character" "frames-without-wrap" "tdf146264" "floating" "spec-fragment" "default-styles" "tdf156093-image" "RedSquare" "tdf156093-image_alignment")
 ODF_VERSION=odf1.4
 TEST_INPUT_FILE=content.xml
 TEST_STYLESHEET=src/test/resources/${ODF_VERSION}/tools/odf2html/export/xhtml/opendoc2xhtml.xsl
